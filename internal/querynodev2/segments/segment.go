26b5cd22fd510a31221ba59cd20592434843d95a
cqy123456 <qianya.cheng@zilliz.com>
2024-07-26 03:45:10 -0500

    cqy debug
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- internal/querynodev2/segments/segment.go
+++ internal/querynodev2/segments/segment.go
@@ -30,6 +30,7 @@ import (
 	"fmt"
 	"io"
 	"runtime"
+	"runtime/debug"
 	"strings"
 	"unsafe"
 
@@ -791,6 +792,7 @@ func (s *LocalSegment) Insert(ctx context.Context, rowIDs []int64, timestamps []
 			(*C.uint8_t)(unsafe.Pointer(&insertRecordBlob[0])),
 			(C.uint64_t)(len(insertRecordBlob)),
 		)
+		debug.FreeOSMemory()
 		return nil, nil
 	}).Await()
 	if err := HandleCStatus(ctx, &status, "Insert failed"); err != nil {
